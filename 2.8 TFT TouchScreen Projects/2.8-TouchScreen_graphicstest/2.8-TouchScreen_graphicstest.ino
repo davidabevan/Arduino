1c0eb857e1491422fad6b9b2101527cccaef003d
Dave Bevan <34312933+davidabevan@users.noreply.github.com>
2018-12-16 13:11:10 +0000

    updates for esp8266 added wiring diagram

--- 2.8 TFT TouchScreen Projects/2.8-TouchScreen_graphicstest/2.8-TouchScreen_graphicstest.ino
+++ 2.8 TFT TouchScreen Projects/2.8-TouchScreen_graphicstest/2.8-TouchScreen_graphicstest.ino
@@ -22,7 +22,25 @@
 // D5              SCK          GPIO14
 // 3.3V            LED                  //*
  
- 
+ //Wiring Diagram
+//****************************************************
+//                             LCD_PIN    Wemos
+//TFT LCD Pin VCC                 *1       5V
+//TFT LCD Pin GND                 *2       GND
+//TFT LCD Pin CS  to GPIO_5       *3       D1
+//TFT LCD Pin RST to RST          *4       RST
+//TFT LCD Pin DC to GPIO_4        *5       D2
+//TFT LCD Pin MOSI to GPIO_13     *6       D7
+//TFT LCD Pin CLK to GPIO_14      *7       D5
+//TFT LCD Pin LED to +3.3 V.      *8       3.3V
+//TFT LCD Pin MISO ( not use )    *9       D6
+//^^^^^^TOUCH^^^^^^^^^^^^^^^^^^^^^^^^
+// TFT LCD PIN T-CLK GPIO14      *10       D5
+// TFT LCD PIN T-CS GPIO0        *11       D3
+// TFT LCD PIN T-DIN GPIO13      *12       D7
+// TFT LCD PIN T-DO GPIO12       *13       D6
+// TFT LCD PIN T-IRQ GPIO2       *14       D4
+//****************************************************
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
