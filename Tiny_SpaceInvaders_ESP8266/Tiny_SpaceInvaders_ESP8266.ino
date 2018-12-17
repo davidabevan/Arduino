87840b2052f2dc0798d4ab5cfa52e0e10b301eb8
Dave Bevan <34312933+davidabevan@users.noreply.github.com>
2018-12-17 13:44:04 +0000

    initial commit
    not compiling graphic problem

--- Tiny_SpaceInvaders_ESP8266/Tiny_SpaceInvaders_ESP8266.ino
+++ Tiny_SpaceInvaders_ESP8266/Tiny_SpaceInvaders_ESP8266.ino
@@ -31,7 +31,7 @@
 #include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
 #include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"`
 #include "OLEDDisplayUi.h"
-
+#include "SSD1306.h"
 // OLED SDA=D3, SCL=D4
 SSD1306Wire  display(0x3c, D4, D3);
 
