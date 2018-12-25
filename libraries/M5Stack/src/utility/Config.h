ada0835435b41c28ed72b2681bd71bcde8f76519
Dave Bevan <34312933+davidabevan@users.noreply.github.com>
2018-12-25 17:57:52 +0000

    Update Config.h

--- libraries/M5Stack/src/utility/Config.h
+++ libraries/M5Stack/src/utility/Config.h
@@ -2,13 +2,14 @@
 #define _CONFIG_H_
 
 // Screen
-#define TFT_LED_PIN 32
-#define TFT_DC_PIN 27
-#define TFT_CS_PIN 14
-#define TFT_MOSI_PIN 23
-#define TFT_CLK_PIN 18
-#define TFT_RST_PIN 33
-#define TFT_MISO_PIN 19
+// adjust to old defaults on ili9341
+#define TFT_LED_PIN   32
+#define TFT_DC_PIN     27//27//4
+#define TFT_CS_PIN    14//14//15
+#define TFT_MOSI_PIN  23
+#define TFT_CLK_PIN   18
+#define TFT_RST_PIN    33//33//2
+#define TFT_MISO_PIN  19
 
 // SD card
 #define TFCARD_CS_PIN 4
