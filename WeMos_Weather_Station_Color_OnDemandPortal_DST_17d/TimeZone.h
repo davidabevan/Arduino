ff8c8a4e12966ad8b57c80767196e211b0219673
Dave Bevan <34312933+davidabevan@users.noreply.github.com>
2020-04-01 09:53:50 +0100

    April 1 2020 UK timezone BST
    3rd attempt at getting neptune dst working for UK

---
+++ WeMos_Weather_Station_Color_OnDemandPortal_DST_17d/TimeZone.h
@@ -0,0 +1,9 @@
+const char TimeZoneConfig[] = R"=====(
+<label><br/>Enter city for time zone settings:<br/>
+Valid values are Boston, New York, Louisville, <br/>
+Chicago, Mountain, Arizona-DST, Arizona-noDST, <br/>
+Pacific, Alaska, Hawaii, Tokyo, UK, and <br/>
+Zurich. <br/>
+Any other entry will cause the time setting to revert to GMT.<br/>
+</label
+)=====";
